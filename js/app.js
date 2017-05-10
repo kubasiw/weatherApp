87e52f3eb0b16974728df7410179eb421b81d3ed
Jakub Siwiński <kubsiw@gmail.com>
2017-05-11 00:36:04 +0200

    add function for box2 that show place with code, flag and date

--- js/app.js
+++ js/app.js
@@ -30,6 +30,16 @@ jQuery(document).ready(function(){
                 var temp = data.main.temp;
                 var pressure = data.main.pressure;
                 var humidity = data.main.humidity;
+                var country = data.sys.country;
+                var countryS = data.sys.country.toLowerCase();
+                
+                var today = new Date();
+                
+                
+                
+                var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+                
+                console.log(date);
                 
                 var descript = data.weather[0].description;
                 var icon = data.weather[0].icon;
@@ -39,14 +49,30 @@ jQuery(document).ready(function(){
                 var div3 = jQuery('section .row div:nth-child(3)');
                 var div4 = jQuery('section .row div:nth-child(4)');
                 
-                console.log(descript);
                 
                 
-                div1.prepend('<div>' + '<img id="icon" src="http://openweathermap.org/img/w/'+icon+'.png" />' + '</div>'
+                function box1() {
+                    div1.prepend('<div>' + '<img id="icon" src="http://openweathermap.org/img/w/'+icon+'.png" />' + '<span>' +temp+ '&deg C' + '</span>' + '</div>'
                             + '<div>' +descript+  '</div>');
 
-                jQuery('section > .row > div:nth-child(1)').addClass('d-flex flex-column align-items-center');
-                jQuery('section > .row > div:nth-child(1) > div:nth-child(2)').addClass('text-center');
+                    jQuery('section > .row > div:nth-child(1)').addClass('d-flex flex-column align-items-center');
+                    jQuery('section > .row > div:nth-child(1) > div:nth-child(1)').addClass('d-flex align-items-center');
+                    jQuery('section > .row > div:nth-child(1) > div:nth-child(2)').addClass('text-center');
+                }
+                box1();
+                
+                function box2() {
+                    
+                    div2.prepend('<div>' + '<span>' + '<img id="flag" src="http://openweathermap.org/images/flags/'+countryS+'.png" />' + '</span>' + ' ' +place+ ', ' + '<span>' +country+ '</span>' + '</div>'
+                                + '<div>' +date+ '</div>');
+                    
+                    jQuery('section > .row > div:nth-child(2)').addClass('d-flex flex-column align-items-center justify-content-around');
+                    
+                    
+                }
+                box2();
+                
+                
                 
             }).fail(function(error) {
                 alert("error");
