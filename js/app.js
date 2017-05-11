0762790914cac3c74e87ae08f90a983086b6c749
Jakub Siwiński <kubsiw@gmail.com>
2017-05-12 01:10:51 +0200

    start to change all the layout because the old one was so awful that I cant focused on coding

--- js/app.js
+++ js/app.js
@@ -30,47 +30,84 @@ jQuery(document).ready(function(){
                 var temp = data.main.temp;
                 var pressure = data.main.pressure;
                 var humidity = data.main.humidity;
+                var wind = data.wind.speed;
+                var windDeg = data.wind.deg+180;
                 var country = data.sys.country;
                 var countryS = data.sys.country.toLowerCase();
                 
+                
                 var today = new Date();
                 
-                
-                
                 var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
-                
-                console.log(date);
                 
                 var descript = data.weather[0].description;
                 var icon = data.weather[0].icon;
                 
-                var div1 = jQuery('section .row div:nth-child(1)');
-                var div2 = jQuery('section .row div:nth-child(2)');
-                var div3 = jQuery('section .row div:nth-child(3)');
-                var div4 = jQuery('section .row div:nth-child(4)');
+                var head = jQuery('header .row div');
                 
+                var unixSunrise = data.sys.sunrise;
+                var unixSunset = data.sys.sunset;
                 
+                var sunriseTime;
+                var sunsetTime;
                 
-                function box1() {
-                    div1.prepend('<div>' + '<img id="icon" src="http://openweathermap.org/img/w/'+icon+'.png" />' + '<span>' +temp+ '&deg C' + '</span>' + '</div>'
-                            + '<div>' +descript+  '</div>');
+                function sunrise() {
+                    // Create a new JavaScript Date object based on the timestamp
+                    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+                    var date = new Date(unixSunrise*1000);
+                    // Hours part from the timestamp
+                    var hours = date.getHours();
+                    // Minutes part from the timestamp
+                    var minutes = "0" + date.getMinutes();
+                    // Seconds part from the timestamp
+                    var seconds = "0" + date.getSeconds();
 
-                    jQuery('section > .row > div:nth-child(1)').addClass('d-flex flex-column align-items-center');
-                    jQuery('section > .row > div:nth-child(1) > div:nth-child(1)').addClass('d-flex align-items-center');
-                    jQuery('section > .row > div:nth-child(1) > div:nth-child(2)').addClass('text-center');
-                }
-                box1();
+                    // Will display time in 10:30:23 format
+                    sunriseTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);  
+                };
+                sunrise();
                 
-                function box2() {
+                function sunset() {
+                    // Create a new JavaScript Date object based on the timestamp
+                    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+                    var date = new Date(unixSunset*1000);
+                    // Hours part from the timestamp
+                    var hours = date.getHours();
+                    // Minutes part from the timestamp
+                    var minutes = "0" + date.getMinutes();
+                    // Seconds part from the timestamp
+                    var seconds = "0" + date.getSeconds();
+
+                    // Will display time in 10:30:23 format
+                    sunsetTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);  
+                };
+                sunset();
+
+                function header() {
+                    head.prepend('<div>' + 'Current weather in ' + ' ' + '<span>' +place+ '</span>' + ', ' + '<span>' +country+ '</span>' + ' ' + '<span>' + '<img id="flag" src="http://openweathermap.org/images/flags/'+countryS+'.png" />' + '</span>' + '</div>');
+                };
+                header();
+                
+                function boxWeather() {
+                    var table = jQuery('tbody');
                     
-                    div2.prepend('<div>' + '<span>' + '<img id="flag" src="http://openweathermap.org/images/flags/'+countryS+'.png" />' + '</span>' + ' ' +place+ ', ' + '<span>' +country+ '</span>' + '</div>'
-                                + '<div>' +date+ '</div>');
+                    table.append('<tr>' + '<td>' + '<img id="icon" src="http://openweathermap.org/img/w/'+icon+'.png" />' + '<span>' +temp+ '&deg C' + '</span>' + '</td>' + '<td>' + '<span>' +descript+ '</span>' + '</td>' + '</tr>');
+                    table.append('<tr>' + '<td>' + 'wilgotność:' + '</td>' + '<td>' +humidity+ '%' + '</td>' + '</tr>');
+                    table.append('<tr>' + '<td>' + 'ciśnienie:' + '</td>' + '<td>' +pressure+ ' hPa' + '</td>' + '</tr>');
+                    table.append('<tr>' + '<td>' + 'wiatr:' + '</td>' + '<td>' +wind+ ' m/s' + '<i class="fa fa-long-arrow-up" aria-hidden="true">' + '</td>' + '</tr>');
+                    table.append('<tr>' + '<td>' + 'wschód:' + '</td>' + '<td>' +sunriseTime+ '</td>' + '</tr>');
+                    table.append('<tr>' + '<td>' + 'zachód:' + '</td>' + '<td>' +sunsetTime+ '</td>' + '</tr>');
                     
-                    jQuery('section > .row > div:nth-child(2)').addClass('d-flex flex-column align-items-center justify-content-around');
+                
+                    var arrow = jQuery('.fa-long-arrow-up');
+                    arrow.css({ WebkitTransform: 'rotate(' + windDeg + 'deg)'})
+                         .css('margin-left', '1em');
                     
+                    jQuery('tbody tr:nth-child(1) > td:nth-child(1)').addClass('d-flex align-items-center');
                     
-                }
-                box2();
+                
+                };
+                boxWeather();
                 
                 
                 
